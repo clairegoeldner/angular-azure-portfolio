d448e9ab6784a6038d63b4709bc8df3583d822fb
Claire Goeldner <crgoeldner@students.ntc.edu>
2024-03-20 15:38:36 -0500

    Update routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule, HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HomeComponent, ResumeComponent, ProjectsComponent, ContactComponent],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  page: string = "home";
+
+  navigate(page: string) : void {
+    this.page = page;
+  }
 }
